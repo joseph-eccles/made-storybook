88eb4d881859492a751d5910763f9e20edfd100b
Joseph Eccles <josepheccles@Administrators-MacBook-Pro.local>
2022-10-01 22:41:02 +0100

    delete storybook

--- storybook/.storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import Made from './Made';
-
-addons.setConfig({
-  theme: Made,
-});
--- storybook/stories/assets/js/global.js
+++
@@ -1,2 +0,0 @@
-//generate a number between 0 to maxInt
-export const uniqueInteger = (maxInt) => Math.floor(Math.random() * maxInt);

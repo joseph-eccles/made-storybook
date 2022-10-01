7e80713e93398d9bbb0290f1b10d929cae9df0bc
Joseph Eccles <josepheccles@Administrators-MacBook-Pro.local>
2022-10-01 22:44:59 +0100

    add storybook

---
+++ storybook/stories/components/01-Core/action-bar/ActionBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const ActionBar = ({
+	items,
+	isDisabled,
+}) => {
+
+	return (
+		<ul class="made-c-action-bar">
+		{items.map(function (label, index) {
+			return (
+				<li class="made-c-action-bar__item" id={index}>
+					<a href="" role="button" 
+						className={
+							isDisabled == true
+								? "made-u-link--disabled"
+								: ""
+						}>
+					{label}
+					</a>
+     			</li>
+			);
+		})}
+	</ul>
+	);
+};
+
+ActionBar.propTypes = {
+	items: PropTypes.objectOf(PropTypes.string),
+	isDisabled: PropTypes.bool,	
+};
+
